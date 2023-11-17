ad587637af91430adc29ab1b836fc5cc7c2191f6
Kim Bogdanov <kimbogdanov@gmail.com>
2023-11-17 14:22:43 +0300

    feat: did new models User, Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+public class Film {
+    Long id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    long duration;
+}
