1aad4a75da39a70883aa7e5925c41bffe2bede72
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2023-12-14 13:06:35 +0300

    refactor: check duplicate

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FilmService {
@@ -24,39 +26,48 @@ public class FilmService {
     }
 
     public Film createFilm(Film film) {
+        List<Genre> genres = film.getGenres();
+        List<Genre> uniqueGenres = genres.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        film.setGenres(uniqueGenres);
         return filmStorage.createFilm(film);
     }
 
     public Film updateFilm(Film film) {
-        if(isExist(film.getId())){
+        if (isExist(film.getId())) {
             throw new EntityNotFoundException("Фильма с id " + film.getId() + " нет в базе");
         }
+        List<Genre> genres = film.getGenres();
+        List<Genre> uniqueGenres = genres.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        film.setGenres(uniqueGenres);
         return filmStorage.updateFilm(film);
     }
 
     public Film getFilm(Long id) {
-        if(isExist(id)){
+        if (isExist(id)) {
             throw new EntityNotFoundException("Фильма с id " + id + " нет в базе");
         }
-
         return filmStorage.getFilmById(id);
     }
 
     public void addLike(Long id, Long userId) {
-        if(isExist(id)){
+        if (isExist(id)) {
             throw new EntityNotFoundException("Фильма с id " + id + " нет в базе");
         }
-        if(userService.isExist(userId)){
+        if (userService.isExist(userId)) {
             throw new EntityNotFoundException("User с id " + userId + " нет в базе");
         }
         filmStorage.addLike(id, userId);
     }
 
     public void deleteLike(Long id, Long userId) {
-        if(isExist(id)){
+        if (isExist(id)) {
             throw new EntityNotFoundException("Фильма с id " + id + " нет в базе");
         }
-        if(userService.isExist(userId)){
+        if (userService.isExist(userId)) {
             throw new EntityNotFoundException("User с id " + userId + " нет в базе");
         }
         filmStorage.deleteLike(id, userId);
@@ -65,7 +76,8 @@ public class FilmService {
     public List<Film> getPopularFilms(Integer count) {
         return filmStorage.getPopularFilms(count);
     }
-    public boolean isExist(Long id){
+
+    public boolean isExist(Long id) {
         return filmStorage.isExist(id);
     }
 }
